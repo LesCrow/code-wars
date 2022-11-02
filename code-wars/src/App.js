acb23c7e5e84895ac521b6506c59e59f388659a8
LesCrow <john.baruteau@gmail.com>
2022-11-02 17:05:08 +0100

    cards order sort and cards design

--- code-wars/src/App.js
+++ code-wars/src/App.js
@@ -1,26 +1,41 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-import DisplayUsers from "./components/DisplayUsers";
+import UserCard from "./components/UserCard";
+import users from "./constants/users";
 
 function App() {
-  const users = [
-    "https://www.codewars.com/api/v1/users/Lescrow",
-    "https://www.codewars.com/api/v1/users/Nicolas-CARRERE",
-    "https://www.codewars.com/api/v1/users/Damsbot64",
-    "https://www.codewars.com/api/v1/users/Pupuce-64",
-    "https://www.codewars.com/api/v1/users/FaannY",
-    "https://www.codewars.com/api/v1/users/Tomahawkiwi",
-    "https://www.codewars.com/api/v1/users/e-genie",
-    "https://www.codewars.com/api/v1/users/MaximeB29",
-    "https://www.codewars.com/api/v1/users/Antoine64160",
-    "https://www.codewars.com/api/v1/users/warasil",
-  ];
+  const [data, setData] = useState([]);
+
+  const getDataFromAllUsers = async () => {
+    await Promise.all(
+      users.map((user) =>
+        axios
+          .get(`https://www.codewars.com/api/v1/users/${user.userName}`)
+          .then((res) => res.data)
+          .then((response) => setData((state) => [...state, response]))
+      )
+    );
+  };
+
+  useEffect(() => {
+    getDataFromAllUsers();
+  }, []);
+
+  console.log(data);
+
+  if (data.length < 1) return <div>Loading ...</div>;
 
   return (
     <div>
-      {users.map((user) => (
-        <DisplayUsers user={user} />
-      ))}
+      <div className="h-full bg-gradient-to-r from-black via- to-red-500">
+        {data
+          .sort((a, b) => b.honor - a.honor)
+          .map((user, index) => (
+            <UserCard index={index} key={user.id} user={user} />
+          ))}
+      </div>
     </div>
   );
 }
